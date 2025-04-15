07d1e3d0c0555bcbd2dfc4060ced5124357498c3
Miriah Peterson <miriah@agilityads.com>
2025-04-15 10:12:33 -0600

    fix: go stuff is weird
    Signed-off-by: Miriah Peterson <miriah@agilityads.com>

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,26 +1,26 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup lazy.nvim
+-- setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  -- install = { colorscheme = { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 }},
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {
+		{ "lazyvim/lazyvim", import = "lazyvim.plugins" },
+		-- import your plugins
+		{ import = "plugins" },
+	},
+	-- configure any other settings here. see the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	-- install = { colorscheme = { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 }},
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
--- nvim/lua/plugins/vimplug.lua
+++ nvim/lua/plugins/vimplug.lua
@@ -2,7 +2,7 @@ return {
 	-- Themes
 
 	-- Go
-	{ "fatih/vim-go", build = ":GoInstallBinaries" },
+	-- { "fatih/vim-go", build = ":GoInstallBinaries" },
 	"buoto/gotests-vim",
 
 	-- LSP
@@ -72,11 +72,6 @@ return {
 				---@type lspconfig.options
 				servers = {
 					lua_ls = {
-						-- mason = false, -- set to false if you don't want this server to be installed with mason
-						-- Use this to add any additional keymaps
-						-- for specific lsp servers
-						-- ---@type LazyKeysSpec[]
-						-- keys = {},
 						settings = {
 							Lua = {
 								workspace = {
@@ -102,18 +97,66 @@ return {
 							},
 						},
 					},
+					gopls = {
+						settings = {
+							gopls = {
+								gofumpt = true,
+								codelenses = {
+									gc_details = false,
+									generate = true,
+									regenerate_cgo = true,
+									run_govulncheck = true,
+									test = true,
+									tidy = true,
+									upgrade_dependency = true,
+									vendor = true,
+								},
+								hints = {
+									assignVariableTypes = true,
+									compositeLiteralFields = true,
+									compositeLiteralTypes = true,
+									constantValues = true,
+									functionTypeParameters = true,
+									parameterNames = true,
+									rangeVariableTypes = true,
+								},
+								analyses = {
+									nilness = true,
+									unusedparams = true,
+									unusedwrite = true,
+									useany = true,
+								},
+								usePlaceholders = true,
+								completeUnimported = true,
+								staticcheck = true,
+								directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+								semanticTokens = true,
+							},
+						},
+					},
 				},
 				-- you can do any additional lsp server setup here
 				-- return true if you don't want this server to be setup with lspconfig
 				---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
 				setup = {
-					-- example to setup with typescript.nvim
-					-- tsserver = function(_, opts)
-					--   require("typescript").setup({ server = opts })
-					--   return true
-					-- end,
-					-- Specify * to use this function as a fallback for any server
-					-- ["*"] = function(server, opts) end,
+					gopls = function(_, opts)
+						-- workaround for gopls not supporting semanticTokensProvider
+						-- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+						LazyVim.lsp.on_attach(function(client, _)
+							if not client.server_capabilities.semanticTokensProvider then
+								local semantic = client.config.capabilities.textDocument.semanticTokens
+								client.server_capabilities.semanticTokensProvider = {
+									full = true,
+									legend = {
+										tokenTypes = semantic.tokenTypes,
+										tokenModifiers = semantic.tokenModifiers,
+									},
+									range = true,
+								}
+							end
+						end, "gopls")
+						-- end workaround
+					end,
 				},
 			}
 			return ret
@@ -259,8 +302,12 @@ return {
 			end
 		end,
 	},
-	-- "williamboman/mason-lspconfig.nvim",
-	-- "williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"williamboman/mason.nvim",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = { ensure_installed = { "go", "gomod", "gowork", "gosum" } },
+	},
 
 	-- Telescope
 	{ "nvim-lua/plenary.nvim" },
