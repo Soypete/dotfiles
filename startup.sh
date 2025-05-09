9424a20815767edfe61a3caa4914530731421df8
Miriah Peterson <soypete@Miriahs-MacBook-Pro.local>
2025-05-09 11:26:13 -0600

    build: aliases and comments
    Signed-off-by: Miriah Peterson <miriah@schoolai.com>

--- startup.sh
+++ startup.sh
@@ -1,29 +1,29 @@
 # /bin/bash
 
-# install ohmyzsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#curl -sS https://webi.sh/webi | sh; \
-#source ~/.config/envman/PATH.env
+curl -sS https://webi.sh/webi | sh
+source ~/.config/envman/PATH.env
 
-#webi brew@stable jq@stable gh@stable terraform@stable go@stable python@stable  rg@stable
+webi brew@stable jq@stable gh@stable terraform@stable go@stable python@stable rg@stable
 
-#curl -LsSf https://astral.sh/uv/install.sh | sh
-#uv tool install ruff@latest
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv tool install ruff@latest
 
-# brew install neovim
+#non-webi tools
+brew install neovim
+brew install podman
 
-# brew install podman
-
-# start podman
-#podman machine init
-#podman machine start
+#start podman
+podman machine init
+podman machine start
 
 touch ~/.secrets
 
-# brew install 1password-cli
-
+brew install 1password-cli
 echo "export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock" >>./zsh/zsh_profile
+
 rm ~/.bashrc
 ln -s dotfiles/bash/bashrc ~/.bashrc
 rm ~/.zshrc
@@ -33,4 +33,5 @@ ln -s dotfiles/zsh/zsh_profile ~/.zsh_profile
 
 source ~/.zshrc
 
+# I like to put all my code stuff here
 mkdir ~/code/
