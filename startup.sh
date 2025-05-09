fed5450524d03268902175bdbef965d73ebd12c9
Miriah Peterson <soypete@Miriahs-MacBook-Pro.local>
2025-05-09 11:22:13 -0600

    build: migrate from bew to webi
    Signed-off-by: Miriah Peterson <soypete@Miriahs-MacBook-Pro.local>

--- startup.sh
+++ startup.sh
@@ -1,44 +1,37 @@
 # /bin/bash
 
-# install ohmyzsh
+install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install brew
--c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/miriah.peterson/dotfiles/zsh/zsh_profile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+curl -sS https://webi.sh/webi | sh
+source ~/.config/envman/PATH.env
 
-brew upgrade
+webi brew@stable jq@stable gh@stable terraform@stable go@stable python@stable rg@stable node@stable
 
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv tool install ruff@latest
+
+#non-webi tools
 brew install neovim
-#install vim plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 brew install podman
+brew install fzf
 
-# start podman
+#start podman
 podman machine init
 podman machine start
 
-brew install jq
-
 touch ~/.secrets
 
-brew install --cask 1password/tap/1password-cli
+brew install 1password-cli
 
-# check if ssh key exists
- echo -e "Checking if you have setup a key for serve ssh connection ..."
-ls -al ~/.ssh | grep id_ed25519.pub 
-if [ $? -eq 0 ]
-then
-git clone git@github.com:Soypete/dotfiles.git
-ln -s dotfiles/bash/bashrc .bashrc
-ln -s dotfiles/zsh/zshrc .zshrc
-ln -s dotfiles/nvim/init.vim .config/nvim/init.vim
+rm ~/.bashrc
+ln -s dotfiles/bash/bashrc ~/.bashrc
+rm ~/.zshrc
+ln -s dotfiles/zsh/zshrc ~/.zshrc
+rm ~/.zshprofile
+ln -s dotfiles/zsh/zsh_profile ~/.zsh_profile
 
-source ~.zshrc
-else
-  echo -e "Set up a ssh key please "
-fi
+source ~/.zshrc
 
+# I like to put all my code stuff here
+mkdir ~/code/
